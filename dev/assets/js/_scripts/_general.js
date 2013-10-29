cb026b0243ada24c69a1592e776eafb65bddace0
LFeh <luizfelipe.tf.13@gmail.com>
2013-10-28 22:39:05 -0200

    mp3 files

--- dev/assets/js/_scripts/_general.js
+++ dev/assets/js/_scripts/_general.js
@@ -7,76 +7,76 @@
   // Notes variables
 	var notes = {
 		'1C': new Howl({
-			urls: ['assets/midia/1C.ogg']
+			urls: ['assets/midia/1C.mp3']
 		}),
 		'1Cs': new Howl({
-			urls: ['assets/midia/1Cs.ogg']
+			urls: ['assets/midia/1Cs.mp3']
 		}),
 		'1D': new Howl({
-			urls: ['assets/midia/1D.ogg']
+			urls: ['assets/midia/1D.mp3']
 		}),
 		'1Ds': new Howl({
-			urls: ['assets/midia/1Ds.ogg']
+			urls: ['assets/midia/1Ds.mp3']
 		}),
 		'1E': new Howl({
-			urls: ['assets/midia/1E.ogg']
+			urls: ['assets/midia/1E.mp3']
 		}),
 		'1F': new Howl({
-			urls: ['assets/midia/1F.ogg']
+			urls: ['assets/midia/1F.mp3']
 		}),
 		'1Fs': new Howl({
-			urls: ['assets/midia/1Fs.ogg']
+			urls: ['assets/midia/1Fs.mp3']
 		}),
 		'1G': new Howl({
-			urls: ['assets/midia/1G.ogg']
+			urls: ['assets/midia/1G.mp3']
 		}),
 		'1Gs': new Howl({
-			urls: ['assets/midia/1Gs.ogg']
+			urls: ['assets/midia/1Gs.mp3']
 		}),
 		'2A': new Howl({
-			urls: ['assets/midia/2A.ogg']
+			urls: ['assets/midia/2A.mp3']
 		}),
 		'2As': new Howl({
-			urls: ['assets/midia/2As.ogg']
+			urls: ['assets/midia/2As.mp3']
 		}),
 		'2B': new Howl({
-			urls: ['assets/midia/2B.ogg']
+			urls: ['assets/midia/2B.mp3']
 		}),
 		'2C': new Howl({
-			urls: ['assets/midia/2C.ogg']
+			urls: ['assets/midia/2C.mp3']
 		}),
 		'2Cs': new Howl({
-			urls: ['assets/midia/2Cs.ogg']
+			urls: ['assets/midia/2Cs.mp3']
 		}),
 		'2D': new Howl({
-			urls: ['assets/midia/2D.ogg']
+			urls: ['assets/midia/2D.mp3']
 		}),
 		'2Ds': new Howl({
-			urls: ['assets/midia/2Ds.ogg']
+			urls: ['assets/midia/2Ds.mp3']
 		}),
 		'2E': new Howl({
-			urls: ['assets/midia/2E.ogg']
+			urls: ['assets/midia/2E.mp3']
 		}),
 		'2F': new Howl({
-			urls: ['assets/midia/2F.ogg']
+			urls: ['assets/midia/2F.mp3']
 		}),
 		'2Fs': new Howl({
-			urls: ['assets/midia/2Fs.ogg']
+			urls: ['assets/midia/2Fs.mp3']
 		}),
 		'2G': new Howl({
-			urls: ['assets/midia/2G.ogg']
+			urls: ['assets/midia/2G.mp3']
 		}),
 		'2Gs': new Howl({
-			urls: ['assets/midia/2Gs.ogg']
+			urls: ['assets/midia/2Gs.mp3']
 		}),
 		'3A': new Howl({
-			urls: ['assets/midia/3A.ogg']
+			urls: ['assets/midia/3A.mp3']
 		}),
 		'3As': new Howl({
-			urls: ['assets/midia/3As.ogg']
+			urls: ['assets/midia/3As.mp3']
 		}),
 		'3B': new Howl({
-			urls: ['assets/midia/3B.ogg']
+			urls: ['assets/midia/3B.mp3']
 		})
 	}; 
 
@@ -93,15 +93,9 @@
 	  ev.preventDefault();
 	  return false;
 	});  
-
-	// Keydown Console
-  // --------------------------------------------------
-	$(window).bind('keydown', function(ev) {
-		console.log(ev.which);
-	}); 
-
+ 
 	// Piano Play Keyboard
-  // --------------------------------------------------
+    // --------------------------------------------------
 	$(window).bind('keydown keyup', function(ev) {
 		var keyNo = ev.which;
 		var $key = $('[data-key="'+keyNo+'"]');
