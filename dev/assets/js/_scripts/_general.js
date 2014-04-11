bd284dd720d40a1f8a9ca6a3c4203abc82dee707
LFeh <luizfelipe.tf.13@gmail.com>
2014-04-11 14:31:40 -0300

    fix notes

--- dev/assets/js/_scripts/_general.js
+++ dev/assets/js/_scripts/_general.js
@@ -6,78 +6,78 @@
 
   // Notes variables
 	var notes = {
-		'1C': new Howl({
-			urls: ['assets/midia/1C.mp3']
-		}),
-		'1Cs': new Howl({
-			urls: ['assets/midia/1Cs.mp3']
-		}),
-		'1D': new Howl({
-			urls: ['assets/midia/1D.mp3']
-		}),
-		'1Ds': new Howl({
-			urls: ['assets/midia/1Ds.mp3']
-		}),
-		'1E': new Howl({
-			urls: ['assets/midia/1E.mp3']
-		}),
-		'1F': new Howl({
-			urls: ['assets/midia/1F.mp3']
-		}),
-		'1Fs': new Howl({
-			urls: ['assets/midia/1Fs.mp3']
-		}),
-		'1G': new Howl({
-			urls: ['assets/midia/1G.mp3']
-		}),
-		'1Gs': new Howl({
-			urls: ['assets/midia/1Gs.mp3']
-		}),
-		'2A': new Howl({
-			urls: ['assets/midia/2A.mp3']
-		}),
-		'2As': new Howl({
-			urls: ['assets/midia/2As.mp3']
-		}),
-		'2B': new Howl({
-			urls: ['assets/midia/2B.mp3']
-		}),
-		'2C': new Howl({
-			urls: ['assets/midia/2C.mp3']
-		}),
-		'2Cs': new Howl({
-			urls: ['assets/midia/2Cs.mp3']
-		}),
-		'2D': new Howl({
-			urls: ['assets/midia/2D.mp3']
-		}),
-		'2Ds': new Howl({
-			urls: ['assets/midia/2Ds.mp3']
-		}),
-		'2E': new Howl({
-			urls: ['assets/midia/2E.mp3']
-		}),
-		'2F': new Howl({
-			urls: ['assets/midia/2F.mp3']
-		}),
-		'2Fs': new Howl({
-			urls: ['assets/midia/2Fs.mp3']
-		}),
-		'2G': new Howl({
-			urls: ['assets/midia/2G.mp3']
-		}),
-		'2Gs': new Howl({
-			urls: ['assets/midia/2Gs.mp3']
-		}),
-		'3A': new Howl({
-			urls: ['assets/midia/3A.mp3']
-		}),
-		'3As': new Howl({
-			urls: ['assets/midia/3As.mp3']
-		}),
-		'3B': new Howl({
-			urls: ['assets/midia/3B.mp3']
-		})
+    "1C": new Howl({
+        urls: [ "assets/midia/261-C.mp3" ]
+    }),
+    "1Cs": new Howl({
+        urls: [ "assets/midia/277-C-sharp.mp3" ]
+    }),
+    "1D": new Howl({
+        urls: [ "assets/midia/293-D.mp3" ]
+    }),
+    "1Ds": new Howl({
+        urls: [ "assets/midia/311-D-sharp.mp3" ]
+    }),
+    "1E": new Howl({
+        urls: [ "assets/midia/329-E.mp3" ]
+    }),
+    "1F": new Howl({
+        urls: [ "assets/midia/349-F.mp3" ]
+    }),
+    "1Fs": new Howl({
+        urls: [ "assets/midia/369F-sharp.mp3" ]
+    }),
+    "1G": new Howl({
+        urls: [ "assets/midia/391-G.mp3" ]
+    }),
+    "1Gs": new Howl({
+        urls: [ "assets/midia/415-G-sharp.mp3" ]
+    }),
+    "2A": new Howl({
+        urls: [ "assets/midia/440-A.mp3" ]
+    }),
+    "2As": new Howl({
+        urls: [ "assets/midia/466-A-sharp.mp3" ]
+    }),
+    "2B": new Howl({
+        urls: [ "assets/midia/495-B.mp3" ]
+    }),
+    "2C": new Howl({
+        urls: [ "assets/midia/523-C.mp3" ]
+    }),
+    "2Cs": new Howl({
+        urls: [ "assets/midia/545-C-sharp.mp3" ]
+    }),
+    "2D": new Howl({
+        urls: [ "assets/midia/587-D.mp3" ]
+    }),
+    "2Ds": new Howl({
+        urls: [ "assets/midia/622-D-sharp.mp3" ]
+    }),
+    "2E": new Howl({
+        urls: [ "assets/midia/659-E.mp3" ]
+    }),
+    "2F": new Howl({
+        urls: [ "assets/midia/698-F.mp3" ]
+    }),
+    "2Fs": new Howl({
+        urls: [ "assets/midia/698-F-sharp.mp3" ]
+    }),
+    "2G": new Howl({
+        urls: [ "assets/midia/783-G.mp3" ]
+    }),
+    "2Gs": new Howl({
+        urls: [ "assets/midia/830-G-sharp.mp3" ]
+    }),
+    "3A": new Howl({
+        urls: [ "assets/midia/880-A.mp3" ]
+    }),
+    "3As": new Howl({
+        urls: [ "assets/midia/932-A-sharp.mp3" ]
+    }),
+    "3B": new Howl({
+        urls: [ "assets/midia/987-B.mp3" ]
+    })
 	};
 
 	// Lock event for play
