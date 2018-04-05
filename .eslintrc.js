540447823cedc1df723e364949d2197eade9cc5e
LFeh <hi@felipefialho.com>
2018-04-05 17:42:37 -0300

    Add lints

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
+  globals: {},
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ['error', 2],
+    'no-unused-vars': [1, { vars: 'local', args: 'none' }],
+    'linebreak-style': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  },
+  env: {
+    node: true
+  }
+};
