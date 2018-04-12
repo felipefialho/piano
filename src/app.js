4d763d0ef6877617e0a683cbad736390060c1b4a
LFeh <hi@felipefialho.com>
2018-04-12 19:51:42 -0300

    Fix imports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-require('./app.styl');
+import * as offline from 'offline-plugin/runtime'; 
+import './app.styl';
+ 
+offline.install({
+  onUpdateReady: function() {
+    offline.applyUpdate();
+  }
+});
