01d4b03c357d0c81aafb7f24c6212517c753467b
felipefialho <hi@felipefialho.com>
2025-03-18 14:22:44 -0300

    chore: remove deprecated offline-plugin

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
-import * as offline from 'offline-plugin/runtime'; 
 import { piano } from './js/piano';
 import './app.styl';
  
 piano();
-offline.install({
-  onUpdateReady: function() {
-    offline.applyUpdate();
-  }
-});
